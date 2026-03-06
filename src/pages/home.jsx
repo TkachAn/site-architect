d2357d3e1878e513d42c6683f1c5e3f594679d81
OksanaTkachevska <61586895+TkachAn@users.noreply.github.com>
2026-03-06 22:58:22 +0200

    edit06032026

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,17 @@
-import React from "react";
-import { Section, Article, Hero } from "../components/stsuct/sections/sections";
-import { InpG } from "../contents/forHome/sec_inp/inpG";
-import { NormButton } from "../components/base/buttons/buttons";
 import { Layout } from "./layout";
 import Catalog from "../contents/forHome/catalog/catalog";
+import HeroSlider from "../contents/forHero/hero";
 
 export function Home() {
   return (
     <Layout>
-      {/* Блок Hero на всю ширину Main */}
+      {/* Блок Hero на всю ширину Main 
       <Hero id="hero" title="Welcome to ARCH.ENGINE">
         <h1>Инженерия будущего</h1>
         <p>Создаем адаптивные интерфейсы с семантической точностью.</p>
         <NormButton>Узнать больше</NormButton>
-      </Hero>
+      </Hero>*/}
+      <HeroSlider />
       <Catalog />
     </Layout>
   );
